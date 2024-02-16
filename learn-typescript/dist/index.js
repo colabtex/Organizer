4f2dbe9fa0145dc8afc3673bbda631947d483f38
Organizer 2024 <organizer2024@outlook.com>
2024-02-15 20:30:59 -0500

    Demonstrate valid declaration and syntax for arrays

--- learn-typescript/dist/index.js
+++ learn-typescript/dist/index.js
@@ -1,6 +1,4 @@
 "use strict";
-let is_published = true;
-let sales = 123456789;
-let course = 'TypeScript';
-let level;
+let valid_num_1 = [1, 2, 3];
+let valid_num_2;
 //# sourceMappingURL=index.js.map
--- learn-typescript/src/index.ts
+++ learn-typescript/src/index.ts
@@ -1,6 +1,8 @@
-let is_published: boolean = true;
+// Not valid, as only arrays of same types allowed when type has been annotated:
+// let invalid_num: number[] = [1, 2, '3'];
 
-// Can remove annotations from variable declarations, since TS can infer types when they are being initialized
-let sales = 123_456_789;
-let course = 'TypeScript';
-let level;
+// Valid, as all elements in array are of the type specified by the type annotation
+let valid_num_1: number[] = [1, 2, 3];
+
+// Also valid, as the elements in the array are all acceptable by an array of the type "any"
+let valid_num_2: [1, 2, '3'];
